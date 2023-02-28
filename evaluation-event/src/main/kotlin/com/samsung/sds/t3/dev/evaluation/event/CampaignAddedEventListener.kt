b1df22abb3173f1b735a18b6b5bbb5ef72cd7f75
kiyoung.you <miroirs01@gmail.com>
2023-02-28 16:56:19 +0900

    api, event 서비스 분리

---
+++ evaluation-event/src/main/kotlin/com/samsung/sds/t3/dev/evaluation/event/CampaignAddedEventListener.kt
@@ -0,0 +1,50 @@
+package com.samsung.sds.t3.dev.evaluation.event
+
+import com.samsung.sds.t3.dev.evaluation.model.CampaignDTO
+import com.samsung.sds.t3.dev.evaluation.service.MessageDataCommandService
+import com.samsung.sds.t3.dev.evaluation.service.SlackMessagingService
+import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.ErrorMessage
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import java.util.function.Consumer
+
+@Component
+class CampaignAddedEventListener (
+    private val messageDataCommandService: MessageDataCommandService,
+    private val slackMessagingService: SlackMessagingService,
+    private val notificationEventPublisher: NotificationEventPublisher
+) {
+
+    private val log: Logger = LoggerFactory.getLogger(this.javaClass)
+
+    @Bean
+    fun campaignAddedEvent(): Consumer<Message<CampaignDTO>> = Consumer { message ->
+
+        runBlocking {
+            val entity = messageDataCommandService.createMessageDataEntity(message)
+            val saved = messageDataCommandService.saveMessageDataEntity(entity)
+            log.info("Saved message:")
+            if (log.isDebugEnabled) log.debug("$saved")
+
+            if (saved.isPass) {
+                notificationEventPublisher.publishNotificationSuccessEvent(saved)
+                slackMessagingService.postMessage(saved)
+            } else {
+                notificationEventPublisher.publishNotificationFailedEvent(saved.hostname!!)
+            }
+        }
+    }
+
+    @Bean
+    fun errorHandler() : Consumer<Mono<ErrorMessage>> = Consumer { message ->
+        message.log()
+            .subscribe {
+                log.info("error occured: \n$it")
+            }
+    }
+}
---
+++ evaluation-event/src/main/kotlin/com/samsung/sds/t3/dev/evaluation/repository/entity/MessageDataEntity.kt
@@ -0,0 +1,23 @@
+package com.samsung.sds.t3.dev.evaluation.repository.entity
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+import java.util.*
+
+@Document(collection = "message_data")
+data class MessageDataEntity (
+    @Id
+    val id: ObjectId? = null,
+    val sentDateTime: LocalDateTime? = null,
+    val hostname: String? = null,
+    val ipAddress: String? = null,
+    val slackUserId: String? = null,
+    val slackUserName: String? = null,
+    val payload: String? = null,
+    val isPass: Boolean = false,
+    @Indexed(unique = true)
+    val uuid: UUID? = null
+) { }
