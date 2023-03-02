ebcc5ab1bdb331340946117539d7a3c765079f00
kiyoung.you <miroirs01@gmail.com>
2023-03-02 15:33:36 +0900

    message consumer flux 이용하도록 구현

--- evaluation-event/src/main/kotlin/com/samsung/sds/t3/dev/evaluation/event/CampaignAddedEventListener.kt
+++ evaluation-event/src/main/kotlin/com/samsung/sds/t3/dev/evaluation/event/CampaignAddedEventListener.kt
@@ -3,14 +3,14 @@ package com.samsung.sds.t3.dev.evaluation.event
 import com.samsung.sds.t3.dev.evaluation.model.CampaignDTO
 import com.samsung.sds.t3.dev.evaluation.service.MessageDataCommandService
 import com.samsung.sds.t3.dev.evaluation.service.SlackMessagingService
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.reactor.mono
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.messaging.Message
 import org.springframework.messaging.support.ErrorMessage
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Mono
+import reactor.core.publisher.Flux
 import java.util.function.Consumer
 
 @Component
@@ -23,25 +23,31 @@ class CampaignAddedEventListener (
     private val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
     @Bean
-    fun campaignAddedEvent(): Consumer<Message<CampaignDTO>> = Consumer { message ->
+    fun campaignAddedEvent(): Consumer<Flux<Message<CampaignDTO>>> = Consumer { message ->
+        message.concatMap {
+            mono { handleMessage(it) }
+        }.onErrorContinue { e, _ ->
+            log.error(e.message, e)
+        }.subscribe()
+    }
 
-        runBlocking {
-            val entity = messageDataCommandService.createMessageDataEntity(message)
-            val saved = messageDataCommandService.saveMessageDataEntity(entity)
-            log.info("Saved message:")
-            if (log.isDebugEnabled) log.debug("$saved")
+    suspend fun handleMessage(message: Message<CampaignDTO>) {
+        val entity = messageDataCommandService.createMessageDataEntity(message)
+        val saved = messageDataCommandService.saveMessageDataEntity(entity)
 
-            if (saved.isPass) {
-                notificationEventPublisher.publishNotificationSuccessEvent(saved)
-                slackMessagingService.postMessage(saved)
-            } else {
-                notificationEventPublisher.publishNotificationFailedEvent(saved.hostname!!)
-            }
+        log.info("Saved message:")
+        if (log.isDebugEnabled) log.debug("$saved")
+
+        if (saved.isPass) {
+            slackMessagingService.postMessage(saved)
+            notificationEventPublisher.publishNotificationSuccessEvent(saved)
+        } else {
+            notificationEventPublisher.publishNotificationFailedEvent(saved.hostname!!)
         }
     }
 
     @Bean
-    fun errorHandler() : Consumer<Mono<ErrorMessage>> = Consumer { message ->
+    fun errorHandler() : Consumer<Flux<ErrorMessage>> = Consumer { message ->
         message.log()
             .subscribe {
                 log.info("error occured: \n$it")
