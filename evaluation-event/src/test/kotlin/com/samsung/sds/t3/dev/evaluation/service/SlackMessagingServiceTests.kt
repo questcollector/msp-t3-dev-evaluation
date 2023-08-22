12efc1861c6ee46ecbe2dff589cf34534f842183
kiyoung.you <miroirs01@gmail.com>
2023-08-22 16:30:14 +0900

    remove unused import

--- evaluation-event/src/test/kotlin/com/samsung/sds/t3/dev/evaluation/service/SlackMessagingServiceTests.kt
+++ evaluation-event/src/test/kotlin/com/samsung/sds/t3/dev/evaluation/service/SlackMessagingServiceTests.kt
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.util.*
-import kotlin.NoSuchElementException
 
 class SlackMessagingServiceTests {
 
