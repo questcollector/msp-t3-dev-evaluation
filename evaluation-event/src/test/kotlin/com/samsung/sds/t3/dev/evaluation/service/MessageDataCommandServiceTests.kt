9dea7769b4c6b4f42ca7fdcfd926a85b3f04170b
kiyoung.you <miroirs01@gmail.com>
2023-08-31 15:06:51 +0900

    private method 테스트케이스로 리팩토링

--- evaluation-event/src/test/kotlin/com/samsung/sds/t3/dev/evaluation/service/MessageDataCommandServiceTests.kt
+++ evaluation-event/src/test/kotlin/com/samsung/sds/t3/dev/evaluation/service/MessageDataCommandServiceTests.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.messaging.MessageHeaders
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.temporal.ChronoUnit
@@ -65,14 +66,13 @@ class MessageDataCommandServiceTests {
 
         runTest {
             val createdMessageDataEntity = messageDataCommandService.createMessageDataEntity(message)
-            val savedMessageDataEntity = messageDataCommandService.saveMessageDataEntity(createdMessageDataEntity)
-            assertThat(savedMessageDataEntity)
+            assertThat(createdMessageDataEntity)
                 .isEqualTo(entity)
         }
     }
 
     @Test
-    fun `통과 여부 계산 테스트`() {
+    fun `isPass = True 테스트`() {
         val message = org.springframework.messaging.support.MessageBuilder
             .withPayload(CampaignDTO())
             .setHeader("InstanceId", "i-02ee325b303f77f4f")
@@ -80,16 +80,99 @@ class MessageDataCommandServiceTests {
             .setHeader("SlackUserId", "id")
             .build()
 
-        coEvery { slackUserInfoService.getSlackUserNameWithSlackUserId("id") } returns "test"
+        val messageDataCommandService = MessageDataCommandService(
+            messageDataRepository,
+            slackUserInfoService
+        )
+
+        val calculateIsPass = messageDataCommandService.javaClass.getDeclaredMethod("calculateIsPass",
+            MessageHeaders::class.java, String::class.java
+        )
+        calculateIsPass.trySetAccessible()
+
+        
+        runTest {
+            val result = calculateIsPass.invoke(messageDataCommandService, message.headers, "test") as Boolean
+            assertThat(result).isTrue
+        }
+    }
+
+    @Test
+    fun `isPass = False, instanceId 없는 경우`() {
+        val message = org.springframework.messaging.support.MessageBuilder
+            .withPayload(CampaignDTO())
+            .setHeader("InstanceId", null)
+            .setHeader("IpAddress", "172.31.19.216")
+            .setHeader("SlackUserId", "id")
+            .build()
 
         val messageDataCommandService = MessageDataCommandService(
             messageDataRepository,
             slackUserInfoService
         )
-        
+
+        val calculateIsPass = messageDataCommandService.javaClass.getDeclaredMethod("calculateIsPass",
+            MessageHeaders::class.java, String::class.java
+        )
+        calculateIsPass.trySetAccessible()
+
+
         runTest {
-            val result = messageDataCommandService.createMessageDataEntity(message)
-            assertThat(result.isPass).isTrue
+            val result = calculateIsPass.invoke(messageDataCommandService, message.headers, "test") as Boolean
+            assertThat(result).isFalse
         }
     }
+
+    @Test
+    fun `isPass = False, ipAddress 없는 경우`() {
+        val message = org.springframework.messaging.support.MessageBuilder
+            .withPayload(CampaignDTO())
+            .setHeader("InstanceId", "i-02ee325b303f77f4f")
+            .setHeader("IpAddress", null)
+            .setHeader("SlackUserId", "id")
+            .build()
+
+        val messageDataCommandService = MessageDataCommandService(
+            messageDataRepository,
+            slackUserInfoService
+        )
+
+        val calculateIsPass = messageDataCommandService.javaClass.getDeclaredMethod("calculateIsPass",
+            MessageHeaders::class.java, String::class.java
+        )
+        calculateIsPass.trySetAccessible()
+
+
+        runTest {
+            val result = calculateIsPass.invoke(messageDataCommandService, message.headers, "test") as Boolean
+            assertThat(result).isFalse
+        }
+    }
+
+    @Test
+    fun `isPass = False, slackUserName 없는 경우`() {
+        val message = org.springframework.messaging.support.MessageBuilder
+            .withPayload(CampaignDTO())
+            .setHeader("InstanceId", "i-02ee325b303f77f4f")
+            .setHeader("IpAddress", "172.31.19.216")
+            .setHeader("SlackUserId", "id")
+            .build()
+
+        val messageDataCommandService = MessageDataCommandService(
+            messageDataRepository,
+            slackUserInfoService
+        )
+
+        val calculateIsPass = messageDataCommandService.javaClass.getDeclaredMethod("calculateIsPass",
+            MessageHeaders::class.java, String::class.java
+        )
+        calculateIsPass.trySetAccessible()
+
+
+        runTest {
+            val result = calculateIsPass.invoke(messageDataCommandService, message.headers, null) as Boolean
+            assertThat(result).isFalse
+        }
+    }
+
 }
