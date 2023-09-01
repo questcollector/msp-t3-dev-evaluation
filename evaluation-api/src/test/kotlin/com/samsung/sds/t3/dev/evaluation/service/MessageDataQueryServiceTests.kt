33bf733da893b3d3afca58398d61407e7c005048
kiyoung.you <miroirs01@gmail.com>
2023-09-01 12:48:14 +0900

    MessageDataQueryServiceTests StepVerifier 도입

--- evaluation-api/src/test/kotlin/com/samsung/sds/t3/dev/evaluation/service/MessageDataQueryServiceTests.kt
+++ evaluation-api/src/test/kotlin/com/samsung/sds/t3/dev/evaluation/service/MessageDataQueryServiceTests.kt
@@ -7,11 +7,14 @@ import io.mockk.coEvery
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.reactor.asFlux
 import kotlinx.coroutines.test.runTest
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import reactor.test.StepVerifier
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import java.util.*
@@ -25,6 +28,7 @@ class MessageDataQueryServiceTests {
 
     private val messageDataRepository = mockk<MessageDataRepository>()
 
+    private val NOW = LocalDateTime.now().withNano(0)
     private val TODAY = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS)
     private val YESTERDAY = TODAY.minusDays(1)
     private val SAMPLE_UUID1 = UUID.randomUUID()
@@ -32,12 +36,20 @@ class MessageDataQueryServiceTests {
 
     @Test
     fun `특정 기간 동안 모든 메시지 조회`() {
-        val entities = flow<MessageDataEntity> {
-            emit(MessageDataEntity(id = SAMPLE_UUID1, sentDateTime = TODAY.minusHours(1)))
-            emit(MessageDataEntity(id = SAMPLE_UUID2, sentDateTime = YESTERDAY.plusHours(1)))
+        val expectEntities = listOf(
+            MessageDataEntity(id = SAMPLE_UUID1, sentDateTime = TODAY.minusHours(1)),
+            MessageDataEntity(id = SAMPLE_UUID2, sentDateTime = YESTERDAY.plusHours(1))
+        )
+
+        val fromRepository = flow {
+            emitAll(expectEntities.asFlow())
+
+            // not in expected result
+            emit(MessageDataEntity(sentDateTime = TODAY.plusHours(1)))
+            emit(MessageDataEntity(sentDateTime = YESTERDAY.minusHours(1)))
         }
 
-        coEvery { messageDataRepository.findAll() } returns entities
+        coEvery { messageDataRepository.findAll() } returns fromRepository
 
         val messageDataQueryService = MessageDataQueryService(messageDataRepository)
 
@@ -45,57 +57,64 @@ class MessageDataQueryServiceTests {
             val result = messageDataQueryService.getMessageDataDuring(
                 YESTERDAY, TODAY
             )
-            println(result.toList())
+            val expect = expectEntities.map { it.toMessageDataDTO() }
 
-            assertThat(result.toList()).containsAll(
-                entities.map {
-                        entity -> entity.toMessageDataDTO()
-                }.toList()
-            )
+            StepVerifier.create(result.asFlux())
+                .expectNext(expect[0], expect[1])
+                .verifyComplete()
         }
 
     }
 
     @Test
     fun `기간 설정 없이 메시지 조회`() {
-        val entities = flow<MessageDataEntity> {
-            emit(MessageDataEntity(id = SAMPLE_UUID1, sentDateTime = TODAY.minusHours(1)))
+        val expectEntities = listOf(
+            MessageDataEntity(id = SAMPLE_UUID1, sentDateTime = TODAY.minusHours(1))
+        )
+
+        val fromRepository = flow {
+            emitAll(expectEntities.asFlow())
+
+            // not in expected result
             emit(MessageDataEntity(id = SAMPLE_UUID2, sentDateTime = YESTERDAY.minusDays(1)))
         }
 
-        coEvery { messageDataRepository.findAll() } returns entities
+        coEvery { messageDataRepository.findAll() } returns fromRepository
 
         val messageDataQueryService = MessageDataQueryService(messageDataRepository)
 
         runTest {
             val result = messageDataQueryService.getMessageDataDuring()
-            assertThat(result.toList())
-                .contains(entities.first().toMessageDataDTO())
-                .doesNotContain(entities.last().toMessageDataDTO())
+
+            val expect = expectEntities.map { it.toMessageDataDTO() }
+
+            StepVerifier.create(result.asFlux())
+                .expectNext(expect[0])
+                .verifyComplete()
         }
 
     }
 
     @Test
     fun `특정 유저의 모든 메시지 조회`() {
-        val NOW = LocalDateTime.now().withNano(0)
-        val entities = flow<MessageDataEntity> {
-            emit(MessageDataEntity(id = SAMPLE_UUID1, sentDateTime = NOW, slackUserName = "test"))
-            emit(MessageDataEntity(id = SAMPLE_UUID2, sentDateTime = NOW, slackUserName = "test"))
-        }
 
-        coEvery { messageDataRepository.findAllBySlackUserNameStartsWith("test") } returns entities
+        val expectedEntities = listOf(
+            MessageDataEntity(id = SAMPLE_UUID1, sentDateTime = NOW, slackUserName = "test1"),
+            MessageDataEntity(id = SAMPLE_UUID2, sentDateTime = NOW, slackUserName = "test2")
+        )
+
+        coEvery { messageDataRepository.findAllBySlackUserNameStartsWith("test") } returns expectedEntities.asFlow()
 
         val messageDataQueryService = MessageDataQueryService(messageDataRepository)
 
         runTest {
             val result = messageDataQueryService.getMessageDataWithSlackUserName(TEST)
 
-            assertThat(result.toList()).containsAll(
-                entities.map {
-                        entity -> entity.toMessageDataDTO()
-                }.toList()
-            )
+            val expect = expectedEntities.map { it.toMessageDataDTO() }
+
+            StepVerifier.create(result.asFlux())
+                .expectNext(expect[0], expect[1])
+                .verifyComplete()
         }
     }
 }
