8968692138d7e7a18a4d2e7b8e403a6be19891af
kiyoung.you <miroirs01@gmail.com>
2024-01-12 18:00:37 +0900

    validator 테스트 케이스 추가

--- evaluation-event/src/test/kotlin/com/github/questcollector/evaluation/event/CampaignAddedEventTests.kt
+++ evaluation-event/src/test/kotlin/com/github/questcollector/evaluation/event/CampaignAddedEventTests.kt
@@ -5,10 +5,8 @@ import com.github.questcollector.evaluation.model.SampleDTO
 import com.github.questcollector.evaluation.repository.entity.MessageDataEntity
 import com.github.questcollector.evaluation.service.MessageDataCommandService
 import com.github.questcollector.evaluation.service.SlackMessagingService
-import io.mockk.coEvery
+import io.mockk.*
 import io.mockk.junit5.MockKExtension
-import io.mockk.mockk
-import io.mockk.spyk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -22,17 +20,19 @@ class CampaignAddedEventTests {
     private val messageDataCommandService = mockk<MessageDataCommandService>()
     private val slackMessagingService = mockk<SlackMessagingService>()
     private val notificationEventPublisher = mockk<NotificationEventPublisher>()
-    private val validator = mockk<SampleDTOMessageValidator>()
+
     @Test
     fun `consumer test`() {
 
-        val campaigns = Flux.just(
+        val messages = Flux.just(
             SampleDTO(id = 1, name = "name1"),
             SampleDTO(id = 2, name = "name2"),
             SampleDTO(id = 3, name = "name3")
         ).map {
             MessageBuilder.withPayload(it).build()
         }
+
+        val validator = mockk<SampleDTOMessageValidator>()
 
         val campaignAddedEventListener = spyk(
             CampaignAddedEventListener(
@@ -43,16 +43,60 @@ class CampaignAddedEventTests {
             )
         )
 
-        coEvery { messageDataCommandService.createMessageDataEntity(any()) } coAnswers {
-            println("handleMessage invoked")
+        // 무조건 통과
+        coEvery { validator.validate(any()) } coAnswers {
             val argumentMessage = this.arg<GenericMessage<SampleDTO>>(0)
-            assertThat(argumentMessage.payload)
-                .matches { it.id in 1 ..< 3 }
-                .matches { it.name in listOf("name1", "name2", "name3") }
+            MessageBuilder.fromMessage(argumentMessage).build()
+        }
+
+        // 이후 작업을 하지 않도록 MessageDataEntity 생성
+        coEvery { messageDataCommandService.createMessageDataEntity(any()) } coAnswers {
+            val argumentMessage = this.arg<GenericMessage<SampleDTO>>(0)
             MessageDataEntity(isPass = false, payload = argumentMessage.payload.toString())
         }
 
-        campaignAddedEventListener.campaignAddedEvent().accept(campaigns)
+        campaignAddedEventListener.campaignAddedEvent().accept(messages)
+
+        // 넘어온 argument로 messages가 잘 넘어왔는지 확인
+        coVerify { messageDataCommandService.createMessageDataEntity(withArg {
+            assertThat(it.payload)
+                .matches { it.id in 1 ..< 3 }
+                .matches { it.name in listOf("name1", "name2", "name3") }
+        }) }
     }
 
+    @Test
+    fun `validator test`() {
+        val messages = arrayOf(
+            SampleDTO(id = 1, name = "name1"),
+            SampleDTO(id = 0, name = "name0"),
+            SampleDTO(id = 2, name = "name2"),
+            SampleDTO(id = 3, name = " ")
+        ).map { MessageBuilder.withPayload(it).build() }.toTypedArray()
+
+        // 이후 작업을 하지 않도록 MessageDataEntity 생성
+        coEvery { messageDataCommandService.createMessageDataEntity(any()) } coAnswers {
+            val message = this.arg<GenericMessage<SampleDTO>>(0)
+            MessageDataEntity(isPass = false, payload = message.payload.toString())
+        }
+
+        // validator는 그대로 사용
+        val campaignAddedEventListener = spyk(
+            CampaignAddedEventListener(
+                messageDataCommandService,
+                slackMessagingService,
+                notificationEventPublisher,
+                SampleDTOMessageValidator()
+            )
+        )
+
+
+        campaignAddedEventListener.campaignAddedEvent().accept(Flux.just(*messages))
+
+        // id가 양수가 아니고 name이 빈 값이 아닌 messages만 들어와야 함
+        coVerify { messageDataCommandService.createMessageDataEntity(withArg {
+            assertThat(it.payload)
+                .matches { it.id !in listOf(0, 3) }
+        }) }
+    }
 }
